48eb6eaee6b3c1c2c2c255536c45fb12d6dbd20e
zehrasbr <zehraisbr@gmail.com>
2023-07-17 20:57:19 +0300

    CoreProject

---
+++ Pizzapan.PresentationLayer/Controllers/LogoutController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Pizzapan.PresentationLayer.Controllers
+{
+    public class LogoutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
